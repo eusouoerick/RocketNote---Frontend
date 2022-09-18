8f5ca345694654d81c576c5854629204f001564c
Samuel <fsantos.samuca@gmail.com>
2022-09-18 14:59:01 -0300

    first commit

---
+++ src/pages/SingIn/index.jsx
@@ -0,0 +1,34 @@
+import { Container, Form, Background } from "./style";
+import { Link } from 'react-router-dom'
+import { FiLock, FiMail } from 'react-icons/fi'
+
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+import { ButtonText } from '../../components/ButtonText'
+
+export function SingIn() {
+    return (
+        <Container>
+            <Form>
+
+                <h1>Rocket Notes</h1>
+                <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+                <h2>Faça seu login</h2>
+
+                <Input icon={FiMail} type="email" placeholder="Email" />
+
+                <Input icon={FiLock} type="password" placeholder="Senha" />
+
+                <Button title="Entrar" />
+
+                <Link to="/register">
+                    <ButtonText isActive title="Criar conta" />
+                </Link>
+
+            </Form>
+
+            <Background />
+
+        </Container>
+    )
+}
